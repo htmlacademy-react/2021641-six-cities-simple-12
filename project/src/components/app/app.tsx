32d75a452816d880323ccecffd778e617240e913
Alexandr136 < email>
2023-03-04 14:25:09 +0300

    Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import SixCities from '../../pages/six-cities/six-cities';
+
+type AppSitiesProps = {
+  offerCount: number;
+}
+
+function App({offerCount}: AppSitiesProps): JSX.Element {
+  return (
+    <SixCities offerCount={offerCount} />
+  );
 }
 
 export default App;
